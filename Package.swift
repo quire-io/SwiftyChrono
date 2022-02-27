d6018c5a3543a61668283b3afefcd2c7801e4654
Baptiste Canton <bat@sbz.fr>
2022-02-28 00:49:40 +0100

    fix compilation by xcode

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.5
 //
 //  Package.swift
 //  SwiftyChrono
@@ -10,5 +11,25 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyChrono",
-    dependencies: []
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SwiftyChrono",
+            targets: ["SwiftyChrono"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "SwiftyChrono",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "SwiftyChronoTests",
+            dependencies: ["SwiftyChrono"]),
+    ]
 )
